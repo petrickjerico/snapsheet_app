70c25e5127bbd3631963cc6efdf425b4bc439466
Petrick Jerico <petrickjerico@gmail.com>
2020-05-27 21:13:22 +0800

    added object models

---
+++ lib/models/category.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  String _categoryTitle;
+  Icon _categoryIcon;
+
+  Category(this._categoryTitle, this._categoryIcon);
+}
---
+++ lib/models/record.dart
@@ -0,0 +1,46 @@
+import 'package:snapsheetapp/models/category.dart';
+
+class Record {
+  String _recordTitle;
+  double _recordValue;
+  Category _recordCategory;
+  String _recordCurrency;
+
+  Record(this._recordTitle, this._recordValue, this._recordCategory,
+      this._recordCurrency);
+
+  String get title {
+    return _recordTitle;
+  }
+
+  double get value {
+    return _recordValue;
+  }
+
+  Category get category {
+    return _recordCategory;
+  }
+
+  String get currency {
+    return _recordCurrency;
+  }
+
+  void rename(String newTitle) {
+    _recordTitle = newTitle;
+  }
+
+  void revalue(double newValue) {
+    _recordValue = newValue;
+  }
+
+  void recategorise(Category newCategory) {
+    _recordCategory = newCategory;
+  }
+
+  void changeCurrency(String newCurrency) {
+    // TODO: change _recordValue to correspond to the given newCurrency.
+    // For now, changeCurrency() only changes the appended currency String
+    // before _recordValue.
+    _recordCurrency = newCurrency;
+  }
+}

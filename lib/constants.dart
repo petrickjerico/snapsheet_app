81af0a3fb20c98919c76af331e838e28574045f8
Joe <jteddysanjaya@gmail.com>
2020-06-14 22:51:04 +0800

    Color Scheme

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+const kBlack = Color(0xFF262834);
+const kLightBlueGrey = Color(0xFFD8F1F5);
+const kCyan = Color(0xFF56CBDC);
+const kDarkCyan = Color(0xFF5991A0);
+const kGrey = Color(0xFFACABAF);
+
 const kStandardStyle = TextStyle(fontSize: 16);
 
 const kHistoryRecordTitle = TextStyle(

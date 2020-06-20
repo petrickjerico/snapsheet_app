c96f17d98c1a67417348ebc5e7683d2064371741
Joe <jteddysanjaya@gmail.com>
2020-06-21 03:26:21 +0800

    Done with receipts preview

--- lib/components/scanner/receipt_screen.dart
+++ lib/components/scanner/receipt_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:snapsheetapp/components/button/delete_button.dart';
 import 'package:snapsheetapp/components/button/rounded_button.dart';
 import 'package:snapsheetapp/components/receipt_image_dialog.dart';
 import 'package:snapsheetapp/config/config.dart';
@@ -35,67 +37,78 @@ class _ReceiptScreenState extends State<ReceiptScreen> {
         UserData userData = Provider.of<UserData>(context);
         Record record = recordView.records[recordId];
         return Scaffold(
+          resizeToAvoidBottomPadding: false,
           backgroundColor: kBlack,
-          body: Theme(
-            data: ThemeData.dark(),
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () async {
-                      await showDialog(
-                          context: context,
-                          builder: (_) => ReceiptImageDialog(record.image));
-                    },
-                    child: Image.file(
-                      record.image,
-                      fit: BoxFit.fitWidth,
-                      height: 150,
-                      width: 800,
+          body: SingleChildScrollView(
+            child: Theme(
+              data: ThemeData.dark(),
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () async {
+                        await showDialog(
+                            context: context,
+                            builder: (_) => ReceiptImageDialog(record.image));
+                      },
+                      child: Image.file(
+                        record.image,
+                        fit: BoxFit.fitWidth,
+                        height: 200,
+                        width: 800,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 30),
-                  // Value + Title
-                  TextFormField(
-                    initialValue: record.title,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Title",
+                    SizedBox(height: 30),
+                    // Value + Title
+                    TextFormField(
+                      initialValue: record.title,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Title",
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          recordView.records[recordId].title = value;
+                        });
+                      },
                     ),
-                    onChanged: (value) {
-                      setState(() {
-                        recordView.records[recordId].title = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    initialValue: record.value.toString(),
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Value",
+                    SizedBox(height: 20),
+                    TextFormField(
+                      initialValue: record.value.toString(),
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Value",
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          recordView.records[recordId].value =
+                              double.parse(value);
+                        });
+                      },
                     ),
-                    onChanged: (value) {
-                      setState(() {
-                        recordView.records[recordId].value =
-                            double.parse(value);
-                      });
-                    },
-                  ),
-                  RoundedButton(
-                    color: Colors.white,
-                    textColor: kBlack,
-                    title: 'Confirm All Receipts',
-                    icon: Icon(Icons.done, color: kBlack),
-                    onPressed: () {
-                      recordView.addAll();
-                      Navigator.pushNamed(context, HomepageScreen.id);
-                    },
-                  )
-                ],
+                    DeleteConfirmButton(
+                        isDelete: record.toDelete,
+                        callBack: () {
+                          setState(() {
+                            recordView.records[recordId].toDelete =
+                                !recordView.records[recordId].toDelete;
+                          });
+                        }),
+                    RoundedButton(
+                      color: Colors.white,
+                      textColor: kBlack,
+                      title: 'Confirm All Receipts',
+                      icon: Icon(Icons.done_all, color: kBlack),
+                      onPressed: () {
+                        recordView.addAll();
+                        Navigator.pushNamed(context, HomepageScreen.id);
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
           ),

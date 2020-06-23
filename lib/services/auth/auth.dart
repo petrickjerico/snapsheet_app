afa367a18272976414f714f8eb495e8b0f2aed60
Joe <jteddysanjaya@gmail.com>
2020-06-23 13:13:06 +0800

    Auth service done

--- lib/services/auth/auth.dart
+++ lib/services/auth/auth.dart
@@ -1,4 +1,8 @@
 abstract class AuthService {
   Future currentUser();
+  Future signInAnon();
+  Future signInWithEmailAndPassword(String email, String password);
+  Future registerWithEmailAndPassword(String email, String password);
+  Future signInWithGoogle();
   Future signOut();
 }

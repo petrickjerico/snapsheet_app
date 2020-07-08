0e6ca21326fbe36cbe30f4186d0702f371d6d5d7
Joe <jteddysanjaya@gmail.com>
2020-07-08 08:27:23 +0800

    Splash Screen

---
+++ lib/ui/shared/splash.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:snapsheetapp/ui/config/config.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Text(
+            'SNAPSHEET',
+            textAlign: TextAlign.center,
+            style: kWelcomeTextStyle.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}

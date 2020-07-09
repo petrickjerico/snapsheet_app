e4056934f96c10132cfe43100932ec95e0d0ee8a
Joe <jteddysanjaya@gmail.com>
2020-07-09 21:17:41 +0800

    Consistent Loading screen color

--- lib/ui/shared/mini_loading.dart
+++ lib/ui/shared/mini_loading.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:snapsheetapp/ui/config/config.dart';
 
-class Loading extends StatelessWidget {
+class MiniLoading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+      color: kBlack,
       child: Center(
         child: SpinKitChasingDots(
           color: Colors.white,

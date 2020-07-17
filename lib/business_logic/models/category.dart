42448f423d453c36b32befd705672cd876ab0970
Joe <jteddysanjaya@gmail.com>
2020-07-17 09:00:59 +0800

    Rework category class

--- lib/business_logic/models/category.dart
+++ lib/business_logic/models/category.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:random_color/random_color.dart';
+import 'package:snapsheetapp/business_logic/default_data/categories.dart';
 
 class Category {
   static final _randomColor = RandomColor();
@@ -18,14 +19,43 @@ class Category {
     this.isIncome = isIncome ?? false;
   }
 
-  Category({
-    this.title,
-    this.icon,
-    this.color,
-    this.isIncome,
-    this.index,
-    this.uid,
-  });
+  Category(
+      {this.title, this.icon, this.color, this.isIncome, this.index, this.uid});
+
+  factory Category.of(Category category) {
+    return Category(
+      title: category.title,
+      icon: category.icon,
+      color: category.color,
+      isIncome: category.isIncome,
+      index: category.index,
+      uid: category.uid,
+    );
+  }
+
+  factory Category.fromFirestore(DocumentSnapshot doc) {
+    Map json = doc.data;
+
+    return Category(
+      title: json['title'],
+      icon: Icon(IconData(json['icon'])),
+      color: Color(json['color']),
+      isIncome: json['isIncome'],
+      index: json['index'],
+      uid: json['uid'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'icon': icon.icon.codePoint,
+      'color': color.value,
+      'isIncome': isIncome,
+      'index': index,
+      'uid': uid
+    };
+  }
 
   @override
   String toString() {

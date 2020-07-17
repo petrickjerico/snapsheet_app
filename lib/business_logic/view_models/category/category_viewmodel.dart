d9f00fa5c822d4e57542d33c9750cff36d32cefd
Joe <jteddysanjaya@gmail.com>
2020-07-17 10:12:54 +0800

    WIP CategoryBaseModel + CategoryViewModel

---
+++ lib/business_logic/view_models/category/category_viewmodel.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:snapsheetapp/business_logic/view_models/category/category_basemodel.dart';
+import 'package:snapsheetapp/business_logic/view_models/user_data_impl.dart';
+
+class CategoryViewModel extends ChangeNotifier implements CategoryBaseModel {
+  UserData userData;
+}
